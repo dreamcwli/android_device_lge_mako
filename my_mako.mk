6aa2e18d6c300ea1d96ff523cfd3893a100f7cfd
Chia-Wei Li <dreamcwli@gmail.com>
2013-08-20 17:23:14 +0800

    Add my_mako
    Change-Id: I6ddf5d2c1126160685425f7da0d0f27e82890f0a

--- my_mako.mk
+++ my_mako.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2012 The Android Open Source Project
+# Copyright 2013 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,8 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+$(call inherit-product, vendor/my/common/common.mk)
+$(call inherit-product, device/lge/mako/full_mako.mk)
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_mako.mk \
-    $(LOCAL_DIR)/full_mako.mk \
-    $(LOCAL_DIR)/my_mako.mk
+PRODUCT_NAME := my_mako
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 4
+
+PRODUCT_NAME_OVERRIDE := occam
